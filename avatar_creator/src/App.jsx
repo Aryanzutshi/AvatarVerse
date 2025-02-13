f3666f5d21efa898cdb2df4c2cc6c996165d602c
Aryan Zutshi <122102323+Aryanzutshi@users.noreply.github.com>
2025-02-13 21:42:30 +0530

    Update App.jsx

--- avatar_creator/src/App.jsx
+++ avatar_creator/src/App.jsx
@@ -1,19 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <>
       <Canvas
         camera={{
-          position: [1, 2, 3],
+          position: [4, 5, 6],
         }}
       >
         <color attach="background" args={["#333333"]} />
         <OrbitControls />
         <mesh>
           <meshNormalMaterial />
-          <boxGeometry args={[1.5, 1.5, 1.5]} />
+          <boxGeometry args={[2, 2, 2]} />
         </mesh>
       </Canvas>
     </>
